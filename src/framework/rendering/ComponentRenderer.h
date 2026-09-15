a547528452e631ce2d818531121aa487e0b6a5c2
Ebdsaleh <nadirsaleh@gmail.com>
2026-09-16 02:22:36 +1000

    Extend renderer contract for styled panels

--- src/framework/rendering/ComponentRenderer.h
+++ src/framework/rendering/ComponentRenderer.h
@@ -5,6 +5,7 @@
 // =================================================================================
 #pragma once
 
+class Panel;
 class Label;
 class Button;
 class TextInput;
@@ -13,6 +14,7 @@ class ComponentRenderer {
     public:
         virtual ~ComponentRenderer() {}
 
+        virtual void render_panel(const Panel& panel) = 0;
         virtual void render_label(const Label& label) = 0;
         virtual void render_button(const Button& button) = 0;
         virtual void render_text_input(const TextInput& text_input) = 0;
